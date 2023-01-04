fdb379f5dd68819a2b6f61a55ea81e85f92a15b4
User1314151 <121974167+User1314151@users.noreply.github.com>
2023-01-04 16:46:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const secondsContainer = document.querySelector('#seconds')
+const minutesContainer = document.querySelector('#minutes')
+const hoursContainer = document.querySelector('#hours')
+const daysContainer = document.querySelector('#days')
+
+const nextYearContainer = document.querySelector('#year')
+const spinnerLoading = document.querySelector('#loading')
+const contdownContainer = document.querySelector('#countdown')
+
+
+
+const nextYear = new Date().getFullYear()
+const newYearTime = new Date(`August 20 ${nextYear} 00:00:00`)
+
+nextYearContainer.textContent = 'Stefhanny'
+
+const getTimeUnit = unit => unit < 10 ? '0' + unit : unit
+
+const insertCountdownValues = ({ days, hours, minutes, seconds }) => {
+    secondsContainer.textContent = getTimeUnit(seconds)
+    minutesContainer.textContent = getTimeUnit(minutes)
+    hoursContainer.textContent = getTimeUnit(hours)
+    daysContainer.textContent = getTimeUnit(days)
+}
+
+const updateCountdown = () => {
+    const currentTime = new Date()
+    const difference = newYearTime - currentTime
+    const days = Math.floor(difference / 1000 / 60 / 60 / 24)
+    const hours = Math.floor(difference / 1000 / 60 / 60) % 24
+    const minutes = Math.floor(difference / 1000 / 60) % 60
+    const seconds = Math.floor(difference / 1000) % 60
+
+    insertCountdownValues({ days, hours, minutes, seconds })
+
+    // console.log({days, hours, minutes, seconds})
+
+
+}
+
+const initialScreen = () => {
+    spinnerLoading.remove()
+
+    contdownContainer.style.display = 'flex'
+}
+
+setTimeout(initialScreen, 1000)
+
+
+setInterval(updateCountdown
+    , 1000)
+
+
